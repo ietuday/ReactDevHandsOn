745565b52584aa87916971aa9411cd5e072a82d9
Udayaditya Singh <udayaditya.singh@gmail.com>
2025-04-06 02:56:08 +0530

    remove coverage thresholds from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -11,13 +11,12 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'json', 'html'],
       exclude: ['node_modules/', 'dist/', 'vite.config.js', 'eslint.config.js'],
-      thresholds: {
-        statements: 90,
-        branches: 90,
-        functions: 90,
-        lines: 90,
-      },
+      // thresholds: {
+      //   statements: 90,
+      //   branches: 90,
+      //   functions: 90,
+      //   lines: 90,
+      // },
     },
-
   },
 });
