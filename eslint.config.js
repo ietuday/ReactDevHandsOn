98435869ce9633f1d77bef4b9b66312b3b8a4116
Udayaditya Singh <udayaditya.singh@gmail.com>
2025-04-06 03:26:19 +0530

    add eslint-plugin-vitest and update ESLint configuration for test files

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,17 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import vitest from 'eslint-plugin-vitest'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage', 'node_modules'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -28,6 +31,29 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    files: ['**/*.test.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest, // not required, but gives IDE love
+        ...globals.commonjs,
+        describe: true,
+        test: true,
+        expect: true,
+        vi: true,
+        beforeEach: true,
+        afterEach: true,
+      },
+    },
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
     },
   },
 ]
